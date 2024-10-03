5565c6b4fa8d9eb190ecc09d921a2232bea62aad
Victor Lorencio <victorlorencio@gmail.com>
2024-10-03 09:10:07 +0200

    Primer commit - Proyecto ZonaCsgApp

---
+++ src/main/java/cs/zona_csg/ZonaCsgApplication.java
@@ -0,0 +1,13 @@
+package cs.zona_csg;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ZonaCsgApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ZonaCsgApplication.class, args);
+	}
+
+}
---
+++ src/test/java/cs/zona_csg/ZonaCsgApplicationTests.java
@@ -0,0 +1,13 @@
+package cs.zona_csg;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ZonaCsgApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
