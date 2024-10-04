7550c7b4f365f9a79c44f66950e5796fb7cb933e
Victor Lorencio <victorlorencio@gmail.com>
2024-10-04 10:28:56 +0200

    Clase presentación ZonaCsg1.0

--- src/main/java/cs/zona_csg/ZonaCsgApplication.java
+++ src/main/java/cs/zona_csg/ZonaCsgApplication.java
@@ -1,13 +1,35 @@
 package cs.zona_csg;
 
+import cs.zona_csg.servicio.IClienteServicio;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class ZonaCsgApplication {
+public class ZonaCsgApplication implements CommandLineRunner {
+
+	@Autowired
+	private IClienteServicio clienteServicio;
+
+	private static final Logger logger = LoggerFactory.getLogger(ZonaCsgApplication.class);//Mandamos información a la consola
 
 	public static void main(String[] args) {
-		SpringApplication.run(ZonaCsgApplication.class, args);
+
+		logger.info("Iniciando la aplicación");
+
+
+
+		SpringApplication.run(ZonaCsgApplication.class, args);//Levantamos la fábrica de objetos de spring
+
+
+		logger.info("Aplicación finalizada");
 	}
 
+	@Override
+	public void run(String... args) throws Exception {
+		logger.info("*** Csg Studio App ***");
+	}
 }
