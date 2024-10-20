66291cc563b1474673bc43c33e3ede3676b5da5f
Victor Lorencio <victorlorencio@gmail.com>
2024-10-20 18:53:01 +0200

    Método en swing para añadir nuevo usuario mediante boton

--- src/main/java/cs/zona_csg/gui/ZonaCsgForma.java
+++ src/main/java/cs/zona_csg/gui/ZonaCsgForma.java
@@ -1,5 +1,6 @@
 package cs.zona_csg.gui;
 
+import cs.zona_csg.modelo.Cliente;
 import cs.zona_csg.servicio.ClienteServicio;
 import cs.zona_csg.servicio.IClienteServicio;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -7,6 +8,8 @@ import org.springframework.stereotype.Component;
 
 import javax.swing.*;
 import javax.swing.table.DefaultTableModel;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 
 
 @Component
@@ -26,18 +29,20 @@ public class ZonaCsgForma extends JFrame{
     public ZonaCsgForma(ClienteServicio clienteServicio){
         this.clienteServicio = clienteServicio;
         iniciarForma();
+        guardarButton.addActionListener(e -> guardarCliente());
     }
 
 
-
+//Inicia el forma de con el panel principal, tamaño y centrado
     private void iniciarForma(){
         setContentPane(panelPrincipal);
         setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         setSize(900, 700);
         setLocationRelativeTo(null);
     }
+//////////////////////////////////////////////////////////////////////
 
-
+//Creación de un modelo de tabla con 4 columnas
     private void createUIComponents() {
         // TODO: place custom component creation code here
         this.tablaModeloClientes = new DefaultTableModel(0, 4);
@@ -48,7 +53,9 @@ public class ZonaCsgForma extends JFrame{
         //Vamos a cargar el listado de clientes
         listarClientes();
     }
+//////////////////////////////////////////////////////////////////////
 
+    //Método para listar los datos de los clientes en tablas
     private void listarClientes(){
         this.tablaModeloClientes.setRowCount(0);//El conteo de registros empieza en 0
         var clientes = this.clienteServicio.listarClientes(); //Vamos a recibir el listado de clientes directamente en la variable creada
@@ -62,4 +69,48 @@ public class ZonaCsgForma extends JFrame{
             this.tablaModeloClientes.addRow(renglonCliente); //Por cada objeto que tengamos lo vamos a ir agregando a nuestra tabla de modelo clientes
         });
     }
+
+//////////////////////////////////////////////////////////////////////
+
+    private void guardarCliente(){
+        if(nombreTexto.getText().equals("")){ //Si no introduce nombre ya que es igual a cadena vacia
+            mostrarMensaje("Proporciona un nombre");
+            nombreTexto.requestFocusInWindow(); //Para dar foco al campo de texto de nombre ya que queremos que sea obligatorio
+            return;
+        }
+        if(membresiaTexto.getText().equals("")){
+            mostrarMensaje("Proporciona un id de membresía");
+            membresiaTexto.requestFocusInWindow();
+            return;
+        }
+        // Recuperamos los valores del formulario
+        var nombre = nombreTexto.getText();
+        var apellido = apellidoTexto.getText();
+        var membresia = Integer.parseInt(membresiaTexto.getText());
+        var cliente = new Cliente(); //ob
+        //El id es un incremento automatico siendo una inserción por lo que lo dejamos como nulo
+        cliente.setNombre(nombre);
+        cliente.setApellido(apellido);
+        cliente.setMembresia(membresia);
+        this.clienteServicio.guardarCliente(cliente); //De esta manera se inserta el nuevo cliente en la base de datos
+        limpiarFormulario();//Una vez que se limpia el formulario hay que recargar la lista de usuarios
+        listarClientes();
+    }
+
+private void mostrarMensaje(String mensaje){
+        JOptionPane.showMessageDialog(this, mensaje);
 }
+
+private void limpiarFormulario(){
+        nombreTexto.setText("");
+        apellidoTexto.setText("");
+        membresiaTexto.setText("");
+}
+
+
+
+}
+
+
+
+
