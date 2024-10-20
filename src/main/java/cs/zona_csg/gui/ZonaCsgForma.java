f8660d4a6e3bffc0cee53c3e0f9c8c7fb8987e3b
Victor Lorencio <victorlorencio@gmail.com>
2024-10-20 18:07:06 +0200

    Función listarClientes para listado de clientes de la tabla

--- src/main/java/cs/zona_csg/gui/ZonaCsgForma.java
+++ src/main/java/cs/zona_csg/gui/ZonaCsgForma.java
@@ -38,5 +38,22 @@ public class ZonaCsgForma extends JFrame{
         String[] cabeceros = {"Id", "Nombre", "Apellido", "Membresia"};
         this.tablaModeloClientes.setColumnIdentifiers(cabeceros);
         this.clientesTabla = new JTable(tablaModeloClientes);
+
+        //Vamos a cargar el listado de clientes
+        listarClientes();
+    }
+
+    private void listarClientes(){
+        this.tablaModeloClientes.setRowCount(0);//El conteo de registros empieza en 0
+        var clientes = this.clienteServicio.listarClientes(); //Vamos a recibir el listado de clientes directamente en la variable creada
+        clientes.forEach(cliente -> {
+            Object[] renglonCliente = { //Creamos un renglon por cada cliente con sus datos correspondientes
+                    cliente.getId(),
+                    cliente.getNombre(),
+                    cliente.getApellido(),
+                    cliente.getMembresia()
+            };
+            this.tablaModeloClientes.addRow(renglonCliente); //Por cada objeto que tengamos lo vamos a ir agregando a nuestra tabla de modelo clientes
+        });
     }
 }
