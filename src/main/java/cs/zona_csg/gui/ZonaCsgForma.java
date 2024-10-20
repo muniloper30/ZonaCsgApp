96202b3ec7d88ab6041ec473f4ec5eca8038ae32
Victor Lorencio <victorlorencio@gmail.com>
2024-10-20 17:45:43 +0200

    Creación de tabla y cabeceros de tabla para el listado de clientes

--- src/main/java/cs/zona_csg/gui/ZonaCsgForma.java
+++ src/main/java/cs/zona_csg/gui/ZonaCsgForma.java
@@ -6,12 +6,15 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
 import javax.swing.*;
+import javax.swing.table.DefaultTableModel;
 
 
 @Component
 public class ZonaCsgForma extends JFrame{
     private JPanel panelPrincipal;
+    private JTable clientesTabla;
     IClienteServicio clienteServicio;
+    private DefaultTableModel tablaModeloClientes;
 
     @Autowired  //Inyección de dependencia
     public ZonaCsgForma(ClienteServicio clienteServicio){
@@ -29,5 +32,11 @@ public class ZonaCsgForma extends JFrame{
     }
 
 
-
+    private void createUIComponents() {
+        // TODO: place custom component creation code here
+        this.tablaModeloClientes = new DefaultTableModel(0, 4);
+        String[] cabeceros = {"Id", "Nombre", "Apellido", "Membresia"};
+        this.tablaModeloClientes.setColumnIdentifiers(cabeceros);
+        this.clientesTabla = new JTable(tablaModeloClientes);
+    }
 }
